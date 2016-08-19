b49efd8f23f04281e235fcc95dcfba7822192e72
pwjablonski Jablonski <pwjablonski@gmail.com>
2016-08-19 20:15:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+$(document).ready(function() {
+  // code to be executed goes here
+
+
+
+});
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function divSelector(){
+  return $('div').slice(0,2)
+}
+
+function firstListItem(){
+  return $('li:first')
+}
