51f9c9814e32975d714d13bf2860341a32ed40dc
Ray Mulligan <rmullig2@optonline.net>
2016-08-28 03:55:31 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  var divs[0] = $('.pic-list');
+  var divs[1] = $('.mocha');
+  return divs
+}
+
+function firstListItem() {
+  return $('#pic-list ul:first-child')
+}
