e0c2b6536678f6d76f05c203c46f658880783188
Christiane Heiligers <tina.heiligers@gmail.com>
2016-09-26 19:10:22 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
-// declare your functions here...
+// $(document).ready(function() {
+  // $('.pics').doSomething
+  // $('#baby-ninja').doSomething
+  // $('ul li').doSomething
+  // $('div img:first-child').doSomething
+  // $("img[alt='the beatles making faces']").doSomething
+  // $('div:last').doSomething
+// })// declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+  // return $('#pic-list ul li:first-child')
+}
