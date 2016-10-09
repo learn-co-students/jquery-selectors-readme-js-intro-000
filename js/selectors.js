de0491f20b775b3f06a0a3e21cade1c264d2c2fd
Rakesh Grewal <grewalrakesh@yahoo.co.in>
2016-10-09 09:52:20 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+ var array =$('p')
+ return array;
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  return $("img[id='baby-ninja']")
+}
+
+function divSelector(){
+  return $('div').slice(0,2)
+}
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
