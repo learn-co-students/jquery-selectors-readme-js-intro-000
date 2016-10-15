f3695abe811823e6bfdd441bc04430328fbdbe82
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2016-10-14 20:42:21 -0400

    Add selector functions

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+
+function paragraphSelector(){
+	var para = $('p')
+	return para;
+}
+
+function lastImageSelector(){
+	var lastImg = $('img:last')
+	return lastImg
+
+}
+
+function ninjaBabySelector(){
+	var bB = $('#baby-ninja')
+	return bB
+}
+
+function divSelector(){
+	var pics = $('.pics')
+	return pics
+}
+
+function firstListItem(){
+	var firstOne = $('#pic-list li:first-child')
+	return firstOne
+}
