3b8cd31cac4eefca874fb2b2cb3da47dbf7be3db
Stephen Penn <penn@stephenpenn.com>
2016-10-23 20:39:17 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('ul img:last')
+
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
