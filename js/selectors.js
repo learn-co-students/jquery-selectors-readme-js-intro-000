58f2333699f11d4ebf1afac2741190003f0a7328
Timothy Flores <tim@timflores.com>
2016-10-24 19:04:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+function paragraphSelector() {
+  return newArray = $('p');
+}
+
+function lastImageSelector() {
+  return newArray = $('img:last');
+}
+
+function ninjaBabySelector() {
+  return newArray = $('#baby-ninja');
+}
+
+function divSelector() {
+  return newArray = $('.pics')
+}
+
+function firstListItem() {
+  return newArray = $('ul#pic-list li:first-child')
+}
+
+// $('parent-tag child-tag:first-child')
+// $('div img:first-child')
