2ba4dcf3ccb4adfbfa5dbdbd1ce6813bafdaaa4d
Ignas nomatter <parazitaz@gmail.com>
2016-11-27 12:21:46 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+function lastImageSelector() {
+  //var a=$('img');
+  //var i= a.length-1;
+  //return a[i];
+  return $('img:last');
+}
+ function ninjaBabySelector(){
+   return $('#baby-ninja');
+ }
+ function divSelector(){
+   return $('div.pics');
+ }
+function firstListItem(){
+  return $('ul li:first-child')
+}
