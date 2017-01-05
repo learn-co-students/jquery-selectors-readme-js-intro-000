85774642f55cf2af84c985f3e933317fd1bdd5e6
Ines Pucker <ines.pucker@gmx.de>
2017-01-05 12:43:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
+function paragraphSelector () {
+return $('p');
+}
+
+function lastImageSelector () {
+return $('li:last-child img');
+}
+
+function ninjaBabySelector () {
+return $('#baby-ninja');
+}
+
+function divSelector () {
+return $('div.pics');
+}
+
+
+function firstListItem () {
+return $('ul#pic-list li:first-child')
+}
+
+
+
+
+
 // declare your functions here...
