9dee17dcc0d4ad30bd05dbaa397e5136b9fb6ead
Muhammad Mehdi <mohummad.mehdi@gmail.com>
2017-01-20 15:28:18 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  selectors1 = $('body p')
+  return selectors1
+}
+
+function lastImageSelector(){
+  var selectors2 = $('body img:last')
+  return selectors2
+}
+function ninjaBabySelector() {
+  var selectors4 = $('#baby-ninja')
+  return selectors4
+}
+
+function divSelector(){
+  var selectors3 = $('.pics')
+  return selectors3
+}
+
+function firstListItem() {
+  var selectors5 = $('#pic-list li:first-child')
+  return selectors5
+}
