4009120b90c6acbf6dbf200700ed7be9f443909a
Alexander Sorochinsky <alexander.sorochinskiy@gmail.com>
2017-02-27 19:04:24 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  var kek = $('p');
+  return kek;
+}
+function lastImageSelector() {
+  var lastSelector = $('img:last');
+  return lastSelector;
+}
+function ninjaBabySelector() {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+function divSelector() {
+  var divSel = $('.pics');
+  return divSel;
+}
+function firstListItem() {
+  var firstItem = $('ul li:first-child');
+  return firstItem;
+}
