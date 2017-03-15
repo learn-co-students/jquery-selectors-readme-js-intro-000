76090727698966b5d4797dfcb27410a121425992
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-03-15 16:28:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector(){
+array =  $('p')
+return array
+}
+
+
+function lastImageSelector(){
+var last_img =  $('img:last')
+return last_img
+}
+
+function ninjaBabySelector(){
+  var ninja = $("#baby-ninja")
+  return ninja
+}
+
+function divSelector(){
+  var pictures = $(".pics")
+  return pictures
+}
+
+function firstListItem(){
+  var first_c = $("ul#pic-list li:first-child")
+  return first_c
+}
