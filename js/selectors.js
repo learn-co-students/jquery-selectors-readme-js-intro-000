cb05dfec1053f4b94d19519b2b3a47d4bfbf9ef6
A H <auh232017@yahoo.com>
2017-04-30 21:03:14 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
-// declare your functions here...
+// var $ = jQuery
+// function paragraphSelector() {
+//   $("p")
+// }
+// //
+// $(function　paragraphSelector() {
+// $('p').text();
+//
+// });
+function paragraphSelector() {
+  return $("p")
+}
+
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $(".pics")
+}
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
