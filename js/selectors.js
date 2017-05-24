8e40dabac90b7a6b232337170a45428b41e21a9f
Manuel Felix <jumanji.felix@gmail.com>
2017-05-24 11:01:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  var arrayOfP = $('p');
+  return arrayOfP;
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('img#baby-ninja');
+}
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
