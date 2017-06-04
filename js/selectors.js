87067571322af8e9be1396595de66ff73a6030e2
Loryleen Batioco <loryleenbatioco@gmail.com>
2017-06-04 00:47:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
-// declare your functions here...
+// note the type of selector: element, class, ID, etc...
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+  //OR
+  //return $('img[src ="https://s3.amazonaws.com/after-school-assets/confused.gif"]')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('#pic-list li:first');
+}
