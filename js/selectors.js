d0479e8b2d2c8295b76d95b1edbdac3c4cc637f9
Nazim Turdiyev <nazik_111@mail.ru>
2017-06-06 09:14:22 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  var lastImg = $('div img:last');
+  return lastImg;
+}
+
+
+function ninjaBabySelector() {
+  var ninjaImgSelector = $('#baby-ninja');
+  return ninjaImgSelector;
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
