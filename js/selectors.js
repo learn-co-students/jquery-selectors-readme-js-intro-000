d6ad2308fb963dbad73f67ab8b2e222737a13927
Neena van de Water <nkuriakose@hotmail.com>
2017-06-28 22:10:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let p = document.getElementsByTagName('p')
+  return p
+}
+
+function lastImageSelector(){
+  let lastImg = $('img:last')
+  return lastImg
+}
+
+function ninjaBabySelector(){
+ let babyNinja = $('#baby-ninja')
+ return babyNinja
+}
+
+function divSelector(){
+  let pics = $('.pics')
+  return pics
+}
+
+function firstListItem(){
+ let firstChild = $('ul#pic-list li:first-child')
+ return firstChild
+}
