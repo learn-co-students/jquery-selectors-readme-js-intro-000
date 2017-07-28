3d2c6a89f6e8df4739529f70d7ab0c4f21f37951
Al Sadik Murtadha <sadiqnoori_2005@yahoo.com>
2017-07-28 01:16:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  var selectById = $('#baby-ninja');
+  return selectById;
+}
+
+function divSelector() {
+  var selectByClass = $('.pics');
+  return selectByClass;
+}
+
+function firstListItem() {
+  var lastPicInIndex = $('#pic-list li:first-child');
+  return lastPicInIndex;
+}
