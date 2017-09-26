2f7f3fcff269eb71847f055c57fa2da93a5d8178
Othman AMD <red.fantastic@hotmail.com>
2017-09-26 17:45:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,20 @@
 // declare your functions here...
 function paragraphSelector(){
-  $('p')
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics, div.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
 }
