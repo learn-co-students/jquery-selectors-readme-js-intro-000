d07d1a0e2c67ed948fe2b0e3e90fde50a9fac596
A. F <aidanf643@gmail.com>
2017-11-25 23:37:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -4,5 +4,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
+  return $('img:last');
+}
 
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
 }
