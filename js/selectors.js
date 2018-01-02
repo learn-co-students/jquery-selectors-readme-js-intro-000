f545b1bb21789d76722fe1c550d86844ccb8edb6
alec <alec@petros.audio>
2018-01-02 21:29:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  var selector = $('p')
+  return selector
+}
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+function ninjaBabySelector () {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+function divSelector() {
+  var divPics = $('.pics');
+  return divPics;
+}
+function firstListItem() {
+  var firstChild = $('ul li:first-child');
+  return firstChild;
+}
