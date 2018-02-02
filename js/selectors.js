1ae5cd6defc7d1b764a4cb145dc2808fc9171538
Mary-Kate James <marykatj@gmail.com>
2018-02-02 15:41:13 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
 // declare your functions here...
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child');
+}
