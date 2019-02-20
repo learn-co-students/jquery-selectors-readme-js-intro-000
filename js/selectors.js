289c1e621b1d1a97383a2f479738b78f5d8eb0c4
lanasgoddard <lanasgoddard@gmail.com>
2019-02-20 13:43:14 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+paragraphSelector(){
+return $('p')
+}
+
+ lastImageSelector(){
+return $('img:last')
+}
+
+ninjaBabySelector(){
+return $('#baby-ninja')
+}
+
+divSelector(){
+return $('.pics')
+}
+
+firstListItem(){
+ return $('#pic-list li :first-child')
+}// declare your functions here...
