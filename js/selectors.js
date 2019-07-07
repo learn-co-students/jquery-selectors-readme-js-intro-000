05ac4a9512825aa852c22c747dea603bae39a90d
Joaquin Gomez <joaquingomezcabido@gmail.com>
2019-07-07 12:48:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+var paragraphSelector = () => {
+  return $('p')
+}
+
+var lastImageSelector = () => {
+  return $('img:last')
+}
+
+var ninjaBabySelector = () => {
+  return $('#baby-ninja')
+}
+
+var divSelector = () => {
+  return $('.pics')
+}
+
+var firstListItem = () => {
+  return $('#pic-list li:first-child')
+}
