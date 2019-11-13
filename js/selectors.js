e45f1919cad97ec2432d52ce31cc348d3f3f8fa6
boisterous-sailor-3516 <christopher.nh.chen@gmail.com>
2019-11-13 23:41:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  let p = $('p');
+  return p;
+}
+
+function lastImageSelector() {
+  let lastImage = $('ul#pic-list li:last-child img');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  let ninjaBaby = $('img#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  let divs = $('.pics');
+  return divs;
+}
+
+function firstListItem() {
+  let first = $('ul#pic-list li:first-child');
+  return first;
+}
