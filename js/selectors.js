dae5f00db44e7d3860ab1b066e8645079237b4de
al <albliu@email.com>
2019-12-09 21:13:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
-// declare your functions here...
+function paragraphSelector(){
+   return $('p')
+}//end paragraphSelector
+
+function lastImageSelector(){
+  return $('img:last')
+}//end lastImageSelector
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+
+}//end ninjaBabySelector
+
+function divSelector(){
+  return $('.pics')
+
+}//end divSelector
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}//end firstListItem
