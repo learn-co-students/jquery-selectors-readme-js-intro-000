1426c8ca566b664771f28352aaf3de1b858fb4c3
Catherine Jimenez <xcatherine.jimenez@gmail.com>
2020-04-09 18:28:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('div img:last');
+}
+// use the last jQuery selector to
+//  return the last image in index.html.
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem (){
+  return $('ul#pic-list li:first-child')
+}
+// use a first-child selector to return the first
+// list item in the ul with the ID pic-list.
