4c7f393461558348fd147da97b03d6b5cb42dc9e
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-08 08:44:47 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
-// declare your functions here...
+//$(document).ready(function(){
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
+
+//})
