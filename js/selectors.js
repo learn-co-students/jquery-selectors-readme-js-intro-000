a82651efc32b1907b765bb635679a8b1eeea99b3
Amy Simpson <simpson.r.amy@gmail.com>
2020-07-16 16:59:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+$(document).ready(
+  
+  function paragraphSelector() {
+  $("p").return("p");
+}
+function lastImageSelector() {
+  $("img").return("img.length-1");
+}
+
+function ninjaBabySelector() {
+  $("#ninja").append("<img src=''/>");  baby image
+}
+function divSelector() {
+  $(".pic").append("pics");   class pics
+}
+function firstListItem() {
+  $("#pic-list").return("p");
+}
+
+);
