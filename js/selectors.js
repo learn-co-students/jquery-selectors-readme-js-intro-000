39af28c110ae526aa16eca761091b5b063b3fd69
Chris <chris.mahfouz@hotmail.com>
2021-03-08 10:00:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector(){
+  let newElement=$('p')
+return newElement
+}
+function lastImageSelector(){
+  let lastImage=$("img:last")
+  return lastImage
+}
+function ninjaBabySelector(){
+  let idSelector=$("#baby-ninja")
+  return idSelector
+}
+
+function divSelector(){
+  let classSelctor=$(".pics")
+  return classSelctor
+}
+function firstListItem() {
+let firstItem=$("ul li:first-child")
+return firstItem
+}
