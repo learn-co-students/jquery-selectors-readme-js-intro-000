091654eaf97da2b6b8b8a85ad96e77b393b27181
Daniel Calvin <arockne2003@yahoo.com>
2021-05-23 01:00:08 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -30,4 +30,36 @@ function firstListItem() {
 //const lastImageSelector = () => $('img:last');
 //const ninjaBabySelector = () => $('#baby-ninja');
 //const divSelector = () => $('.pics');
+//const firstListItem = () => $('ul#pic-list li:first-child');// declare your functions here...
+$('img');
+$('.pics');
+$('#baby-ninja');
+$('ul li');
+$('div img:first-child');
+$('img[alt="the beatles making faces"]');
+$('div:last');
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
+//const paragraphSelector = () => $('p');
+//const lastImageSelector = () => $('img:last');
+//const ninjaBabySelector = () => $('#baby-ninja');
+//const divSelector = () => $('.pics');
 //const firstListItem = () => $('ul#pic-list li:first-child');
